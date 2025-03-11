1dd3f422c94548cbeee23dcdd2f9a886fd112283
Soulittude <79145776+Soulittude@users.noreply.github.com>
2025-03-11 09:55:34 +0300

    Clean Base

--- src/api/user.api.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from './apiClient';
-import { AuthResponse } from '../types/user';
-
-export const login = async (): Promise<AuthResponse> => {
-    return {} as AuthResponse; // Temporary
-};
-export const register = async () => { };
-export const getProfile = async () => { };
