3b7bc16ed55d8b2b9e67d6ea782e14524a9f5057
Soulittude <79145776+Soulittude@users.noreply.github.com>
2025-03-17 23:21:41 +0300

    ProtectedRoute Style Update

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,20 @@
-import { JSX, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { Box, CircularProgress, Typography } from '@mui/material';
 
-export const ProtectedRoute = ({ children }: { children: ReactNode }): JSX.Element => {
+export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
     const { user, loading } = useAuth();
 
     if (loading) {
-        return <div>Loading...</div>;
+        return (
+            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+                <CircularProgress size={60} />
+                <Typography variant="h6" ml={2}>
+                    Loading...
+                </Typography>
+            </Box>
+        );
     }
 
     return user ? <>{children}</> : <Navigate to="/login" replace />;
